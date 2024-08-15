d7e69e7b370fc53cfc78c8e5d48601f71d959687
mosse692 <40247330+mosse692@users.noreply.github.com>
2024-08-15 16:06:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const authRoutes = require('./routes/auth.js')
 
 app.use(cors({
     origin: ["https://chat-app-client-jet.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
 }))
 app.use(express.json())
 // app.use(express.urlencoded())
